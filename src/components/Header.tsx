ad4454aca92e5c484893b229373ba3ba79197588
shomn1koff <lexxminer@gmail.com>
2025-07-06 19:58:13 +0300

    склонения имен

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     <FadeInSection>
       <section className="w-full py-12 bg-pastel1 text-center">
         <h1 className="text-4xl md:text-5xl font-light font-playfair tracking-wide leading-snug">
-          Свадьба Алексей и Алёна
+          Свадьба Алексея и Алёны
         </h1>
         <p className="text-lg md:text-xl mt-2 font-light font-playfair text-gray-700">
           5 августа 2025
