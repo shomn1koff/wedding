a293871b488243530c3c447afceeeb9252f1499f
shomn1koff <lexxminer@gmail.com>
2025-07-07 00:46:02 +0300

    фотощки

--- src/components/MapBlock.tsx
+++ src/components/MapBlock.tsx
@@ -6,12 +6,12 @@ const MapBlock = () => {
 
   return (
     <FadeInSection>
-      <section className="w-full py-10 bg-pastel1 text-center px-4">
+      <section className="w-full py-10 bg-pastel2 text-center px-4">
         <h2 className="text-2xl font-medium font-playfair mb-4">
           Место проведения
         </h2>
 
-        <div className="w-full max-w-2xl mx-auto h-64 rounded-lg overflow-hidden shadow-md">
+        <div className="w-full max-w-md mx-auto h-64 rounded-lg overflow-hidden shadow-md">
           <YMaps>
             <Map
               defaultState={{ center: coordinates, zoom: 16 }}
@@ -23,7 +23,7 @@ const MapBlock = () => {
             </Map>
           </YMaps>
         </div>
-        <p className="mt-2 text-lg md:text-xl font-light font-playfair text-gray-800 leading-relaxed tracking-wide max-w-3xl mx-auto">
+        <p className="mt-2 max-w-md text-lg md:text-xl font-light font-playfair text-gray-800 leading-relaxed tracking-wide mx-auto">
           Адрес: Кабинет и квартира В.И. Ленина, 12 км от МКАД. КПП №3!
         </p>
       </section>
--- src/components/VenueBlock.tsx
+++ src/components/VenueBlock.tsx
@@ -1,7 +1,7 @@
 import VenueCarousel from "./VenueCarousel";
 
 const VenueBlock = () => (
-  <section className="bg-pastel2 py-12 px-4 text-center">
+  <section className="bg-pastel2 pt-4 pb-12 px-4 text-center">
     <VenueCarousel />
   </section>
 );
